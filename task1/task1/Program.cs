39b52c4618e42db4a0ce3954e6f6a59bc14037f7
nada moussa <158825497+nada2710@users.noreply.github.com>
2024-04-07 05:41:26 +0200

    Add files via upload

---
+++ task1/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("please enter num1");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter num2");
+            num2=int.Parse(Console.ReadLine());
+            int r = num1+num2;
+            Console.WriteLine($"sum = {r}");
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    public class rectangle
+    {
+        public int length,width;
+        public void area()
+        {
+            int a = length*width;
+            Console.WriteLine($"area = {a}");
+        }
+        public void perimeter()
+        {
+            int p = (length+width)*2;
+            Console.WriteLine($"perimeter = {p}");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle rec1 = new rectangle();
+            Console.WriteLine("please enter width ");
+            rec1.width=int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter length ");
+            rec1.length=int.Parse(Console.ReadLine());
+            rec1.area();
+            rec1.perimeter();
+        }
+    }
+}
